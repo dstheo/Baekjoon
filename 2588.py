4e38e49b526b7bdf72fc67954b6acf4f6e706c33
Theo <theo963000@gmail.com>
2024-06-21 15:13:38 +0900

    Solve 2558, 2588, 9498, 10171, 10172, 11382

---
+++ 2588.py
@@ -0,0 +1,11 @@
+num1 = int(input())
+num2 = input()
+
+a = num1 * int(num2[2])
+b = num1 * int(num2[1])
+c = num1 * int(num2[0])
+
+print(a)
+print(b)
+print(c)
+print(a + b * 10 + c * 100)
---
+++ 2588_short.py
@@ -0,0 +1,2 @@
+a,b=open(0)
+for x in*b[2::-1],b:print(int(a)*int(x))
